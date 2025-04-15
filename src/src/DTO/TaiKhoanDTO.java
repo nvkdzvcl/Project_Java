b9d927031cf83a6f4c632e8ea860feff80db4fbd
KhanggCloud <vuhongvinhkhang@gmail.com>
2025-04-15 12:44:43 +0700

    Add files via upload

---
+++ src/src/DTO/TaiKhoanDTO.java
@@ -0,0 +1,41 @@
+package DTO;
+
+public class TaiKhoanDTO {
+
+    private int maNV;
+    private String tenNguoiDung;
+    private String matKhau;
+
+    public TaiKhoanDTO(){
+
+    }
+    public TaiKhoanDTO(int maNV, String tenNguoiDung, String matKhau) {
+        this.maNV = maNV;
+        this.tenNguoiDung = tenNguoiDung;
+        this.matKhau = matKhau;
+    }
+
+    public int getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(int maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getTenNguoiDung() {
+        return tenNguoiDung;
+    }
+
+    public void setTenNguoiDung(String tenNguoiDung) {
+        this.tenNguoiDung = tenNguoiDung;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+}
