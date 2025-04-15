6cbcc6f71a3328e5110e741e3ec643846dd8a797
Nguyễn Văn Khanh <3123560042@sv.sgu.edu.vn>
2025-04-15 11:04:52 +0700

    ket noi database

--- src/src/config/JDBCUtil.java
+++ src/src/config/JDBCUtil.java
@@ -1,4 +1,4 @@
-package LapTrinhJava.JavaSwing.Week9;
+package config;
 
 import javax.swing.*;
 import java.sql.Connection;
@@ -8,28 +8,43 @@ public class JDBCUtil {
 
     public static Connection startConnection(){
         Connection conn = null;
-        try{
-            String url = "jdbc:mysql://localhost:3306/java";
+        try {
+            String url = "jdbc:mysql://localhost:3306/quanlycuahangbanquanao";
             String username = "root";
             String password = "";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
             conn = DriverManager.getConnection(url, username, password);
         }
         catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Không thể kết nối với Database");
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Không thể kết nối với Database","Lỗi",JOptionPane.ERROR_MESSAGE);
         }
 
         return conn;
     }
 
-    public static void closeConnection(Connection conn){
-        try{
-            if(conn != null){
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
                 conn.close();
             }
         }
-        catch(Exception e){
-            System.out.println(e);
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        Connection conn = startConnection();
+
+        if (conn != null) {
+            JOptionPane.showMessageDialog(null, "Kết nối database thành công!", "Thành công", JOptionPane.INFORMATION_MESSAGE);
+            closeConnection(conn);
+        } else {
+            JOptionPane.showMessageDialog(null, "Kết nối database thất bại!", "Thất bại", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
 }
